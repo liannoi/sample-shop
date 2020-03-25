d0438be2ab6e9218703505364dd185136e98f5e6
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-03-25 15:38:53 +0200

    Step One - install the packages and to describe models

---
+++ Src/Clients/WebUI/Identity/Models/AppRole.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Shop.WebUI.Identity.Models
+{
+    public class AppRole : IdentityRole
+    {
+        public AppRole() {}
+
+        public AppRole(string name) : base(name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Src/Clients/WebUI/Identity/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Shop.WebUI.Identity.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birhtday { get; set; }
+    }
+}
