31242e3267d0eca9db7c9a3156ea4ab341c95832
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-03-25 20:16:56 +0200

    Added everything else needed for authentication to work correctly

---
+++ Src/Clients/WebUI/Controllers/Identity/AdministratorIdentityController.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Shop.WebUI.Identity;
+using Shop.WebUI.Identity.Models;
+using Shop.WebUI.Identity.ViewModels;
+
+namespace Shop.WebUI.Controllers.Identity
+{
+    public class AdministratorIdentityController : IdentityController
+    {
+        [HttpGet]
+        public ViewResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid) return View(register);
+
+            var userManager = HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+            var user = new AppUser {UserName = register.Email, Email = register.Email};
+
+            var result = await userManager.CreateAsync(user, register.Password);
+            if (result.Succeeded)
+            {
+                HttpContext.GetOwinContext().Authentication.SignIn(
+                    new AuthenticationProperties {IsPersistent = false},
+                    userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie));
+                userManager.AddToRole(userManager.FindByName(register.Email).Id, Consts.AdministratorRoleName);
+                return Redirect(Url.Action("Index", "GoodsFind"));
+            }
+
+            AddErrors(result);
+
+            return View(register);
+        }
+
+        [HttpGet]
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid) return View(login);
+
+            var userManager = HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+
+            var user = userManager.Find(login.Email, login.Password);
+            if (user != null)
+            {
+                HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties {IsPersistent = false},
+                    userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie));
+                return Redirect(Url.Action("Index", "GoodsFind"));
+            }
+
+            ModelState.AddModelError("", "Invalid username or password");
+
+            return View(login);
+        }
+    }
+}
---
+++ Src/Clients/WebUI/Controllers/Identity/IdentityController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Shop.WebUI.Identity.Models;
+
+namespace Shop.WebUI.Controllers.Identity
+{
+    public class IdentityController : Controller
+    {
+        #region Identity (non actions)
+
+        [HttpGet]
+        public RedirectResult CreateRole(string roleName)
+        {
+            var roleManager = HttpContext.GetOwinContext().GetUserManager<RoleManager<AppRole>>();
+            if (!roleManager.RoleExists(roleName))
+                roleManager.Create(new AppRole(roleName));
+            return Redirect(Url.Action("Index", "GoodsFind"));
+        }
+
+        #endregion
+
+        #region Helpers
+
+        protected void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors) ModelState.AddModelError("", error);
+        }
+
+        #endregion
+    }
+}
