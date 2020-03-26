a083a26dc2219efd633c32fa3d1ab96b2876e554
Maksym Liannoi <51001083+liannoi@users.noreply.github.com>
2020-03-26 08:54:08 +0200

    Described role additions when creating a database (if necessary)

--- Src/Clients/WebUI/Consts.cs
+++ Src/Clients/WebUI/Consts.cs
@@ -6,5 +6,6 @@
         public static string SessionCartName => "Cart";
         public static string AdministratorRoleName => "Admin";
         public static string UserRoleName => "User";
+        public static string RootUrl => "http://localhost:54606";
     }
 }
--- Src/Clients/WebUI/Identity/ShopIdentityContext.cs
+++ Src/Clients/WebUI/Identity/ShopIdentityContext.cs
@@ -1,15 +1,43 @@
 ﻿using System.Data.Entity;
+using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Shop.WebUI.Identity.Models;
 
 namespace Shop.WebUI.Identity
 {
+    public class ShopIdentityContextInitializer : DropCreateDatabaseIfModelChanges<ShopIdentityContext>
+    {
+        protected override async void Seed(ShopIdentityContext context)
+        {
+            await Task.Factory.StartNew(SeedAsync);
+            base.Seed(context);
+        }
+
+        #region Helpers
+
+        // ReSharper disable once MemberCanBeMadeStatic.Local
+        private async Task SeedAsync()
+        {
+            await SendCreateRoleRequest(Consts.UserRoleName);
+            await SendCreateRoleRequest(Consts.AdministratorRoleName);
+        }
+
+        // ReSharper disable once MemberCanBeMadeStatic.Local
+        private async Task SendCreateRoleRequest(string roleName)
+        {
+            await new HttpClient().GetAsync($"{Consts.RootUrl}/Identity/CreateRole?roleName={roleName}");
+        }
+
+        #endregion
+    }
+
     public class ShopIdentityContext : IdentityDbContext<AppUser>
     {
         public ShopIdentityContext()
         {
             if (!Database.Exists())
-                Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ShopIdentityContext>());
+                Database.SetInitializer(new ShopIdentityContextInitializer());
         }
     }
 }
